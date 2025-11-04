4fc9b3ea62a746f8209d0317689cd4fe6c7bf601
김진명 <74550514+tobe-honest@users.noreply.github.com>
2025-11-05 02:04:09 +0900

    [S-W3] PG_42835

---
+++ 3주차/PG_42835/PG_42835_김진명.py
@@ -0,0 +1,12 @@
+def solution(people, limit):
+    people.sort()
+    l, r = 0, len(people) - 1
+    answer = 0
+
+    while l <= r:
+        if people[l] + people[r] <= limit:
+            l += 1
+        r -= 1
+        answer += 1
+        
+    return answer
